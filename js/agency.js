791319f9665d688f4b2d68c2cdd09c349d041fa1
Kornienko Denis <korney197823@gmail.com>
2017-02-09 16:30:05 +0300

    Update agency.js

--- js/agency.js
+++ js/agency.js
@@ -46,22 +46,22 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-// Async contact form
-$('form[id=contactForm]').submit(function(){
-  $.post($(this).attr('action'), $(this).serialize(), function(res){
-    $('form[id=contactForm] #success').hide();
-    $('form[id=contactForm] #error').hide();
-    if (res.code == "200")
-      $('form[id=contactForm] #success').show();
-    }).fail(function(){
-    $('form[id=contactForm] #success').hide();
-    $('form[id=contactForm] #error').hide();
-    $('form[id=contactForm] #error').show();
-  });
-  return false;
-});
+// // Async contact form
+// $('form[id=contactForm]').submit(function(){
+//   $.post($(this).attr('action'), $(this).serialize(), function(res){
+//     $('form[id=contactForm] #success').hide();
+//     $('form[id=contactForm] #error').hide();
+//     if (res.code == "200")
+//       $('form[id=contactForm] #success').show();
+//     }).fail(function(){
+//     $('form[id=contactForm] #success').hide();
+//     $('form[id=contactForm] #error').hide();
+//     $('form[id=contactForm] #error').show();
+//   });
+//   return false;
+// });
 
-// Contact form validation
-$.validate({
-  modules : 'html5, toggleDisabled'
-});
+// // Contact form validation
+// $.validate({
+//   modules : 'html5, toggleDisabled'
+// });
